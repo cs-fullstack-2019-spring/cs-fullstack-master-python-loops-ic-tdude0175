064dd3d53f38295da520fd6141cee27198b30421
tdude0175 <tdude0175@gmail.com>
2019-01-18 14:27:48 -0600

    completed 0 2-4

---
+++ forloops_ic.py
@@ -0,0 +1,73 @@
+def main():
+    # problem0()
+    # problem1()
+    # problem2()
+    # problem4()
+    problem5()
+
+'''
+Print Loop
+'''
+
+
+def problem0():
+    print("LOOP")
+
+'''
+Create a program that takes user input and prints the user input.
+'''
+
+def problem1():
+    print(input("i'll repeat anything you say."))
+
+'''
+Create a program that takes user input in a while loop. 
+If they enter ‘q’ or 0 (your choice), quit. 
+Inside the loop print LOOP
+'''
+
+
+def problem2():
+    user_input = input("gimme a word, sentence, anything!")
+
+    for loop in user_input:
+        print(loop)
+        if loop == "q":
+            print("loop!")
+            break
+
+'''
+Create a program that takes user input in a while loop. 
+If they enter 1, print ONE. 
+If they enter 2, print TWO. 
+If they enter 3 print 3. 
+If they enter ‘q’ or 0 (your choice), quit. 
+Else, print ERROR.
+'''
+
+def problem4():
+    userInput = input("gimme a number between 1 and 3")
+    while userInput != "q":
+        if userInput == "1":
+            print("ONE")
+        elif userInput == "2":
+            print("TWO")
+        elif userInput == "3":
+            print("3")
+        else:
+            print("THAT'S NOT RIGHT!")
+        userInput = input("gimme a different one!")
+'''
+Create a program that takes user input. 
+If they enter 1, call a function that prints IN FUNCTION 1. 
+If they press 2, call a function that prints IN FUNCTION 2. 
+If they press 3, call a function that prints IN FUNCTION 3. 
+If they enter ‘q’ or 0 (your choice), quit. 
+Else, print ERROR.
+'''
+
+def problem5():
+    userInput = input("gimme a number between 1-3")
+
+if __name__=='__main__':
+    main()
